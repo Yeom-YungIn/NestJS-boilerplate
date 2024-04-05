29a42b930919bb4fc299410b24cb9c39e6f722be
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-04-05 18:37:16 +0900

    feat: nestjs auth.config set

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import appConfig from "./config/app.config";
 import {ConfigModule} from "@nestjs/config";
+import authConfig from "./auth/config/auth.config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [
-        appConfig
+        appConfig,
+        authConfig
       ],
       envFilePath: ['env/.env.production', 'env/.env.development'],
     }),

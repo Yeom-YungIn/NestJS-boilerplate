5b5cea9b05599c1ebf4223273a862a036ca7039b
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-04-09 23:45:45 +0900

    feat: user Module add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import appConfig from "./config/app.config";
 import {ConfigModule} from "@nestjs/config";
+import { UsersModule } from './users/users.module';
 import authConfig from "./auth/config/auth.config";
 
 @Module({
@@ -14,7 +15,8 @@ import authConfig from "./auth/config/auth.config";
       ],
       envFilePath: ['env/.env.production', 'env/.env.development'],
     }),
-    AuthModule
+    AuthModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],

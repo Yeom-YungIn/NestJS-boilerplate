b425d3ac60be18f03adf2cab5b275b23aebfea46
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2024-04-11 23:59:38 +0900

    refactor: annotation 추가

--- src/users/domain/user.ts
+++ src/users/domain/user.ts
@@ -1,12 +1,18 @@
+import {Exclude, Expose} from "class-transformer";
+
 export class User {
     id: number | string;
 
+    @Expose({groups: ['me', 'admin']})
     email: string | null;
 
+    @Exclude({toPlainOnly: true})
     password: string;
 
+    @Exclude({toPlainOnly: true})
     previousPassword?: string;
 
+    @Expose({groups: ['me', 'admin']})
     provide: string;
 
     socialId?: string | null;
